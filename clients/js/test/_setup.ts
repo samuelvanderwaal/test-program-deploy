373f795977c7e200b9db06e2c9d6838426c5bb32
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2023-05-11 12:44:27 -0800

    fix code formatting

--- clients/js/test/_setup.ts
+++ clients/js/test/_setup.ts
@@ -2,5 +2,4 @@
 import { createUmi as basecreateUmi } from '@metaplex-foundation/umi-bundle-tests';
 import { program } from '../src';
 
-export const createUmi = async () =>
-  (await basecreateUmi()).use(program());
+export const createUmi = async () => (await basecreateUmi()).use(program());
